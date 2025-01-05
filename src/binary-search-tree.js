e0399848fba26d14f3c1c608f79744fd166d48a7
Rixels <mikey.drozd@gmail.com>
2025-01-05 16:20:01 +0300

    feat: task solved

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,40 +7,102 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  constructor() {
+    this.rootHelper = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootHelper;
   }
-
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const addHelper = (helpr, data) => {
+      if (!helpr) return { 
+        data, 
+        left: null, 
+        right: null
+       };
+      if (data < helpr.data) {
+        helpr.left = addHelper(helpr.left, data);
+      } else if (data > helpr.data) {
+        helpr.right = addHelper(helpr.right, data);
+      }
+      return helpr;
+    };
+    this.rootHelper = addHelper(this.rootHelper, data);
   }
-
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    const searchHelper = (helpr, data) => {
+      if (!helpr) {
+        return false;
+      }
+      if (data === helpr.data) {
+        return true;
+      }
+      return data < helpr.data
+        ? searchHelper(helpr.left, data)
+        : searchHelper(helpr.right, data);
+    };
+    return searchHelper(this.rootHelper, data);
   }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    const locateHelper = (helpr, data) => {
+      if (!helpr) {
+        return null;
+      }
+      if (data === helpr.data) {
+        return helpr;
+      }
+      return data < helpr.data
+        ? locateHelper(helpr.left, data)
+        : locateHelper(helpr.right, data);
+    };
+    return locateHelper(this.rootHelper, data);
   }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    const deleteHelper = (helpr, data) => {
+      if (!helpr) {
+        return null;
+      }
+      if (data < helpr.data) {
+        helpr.left = deleteHelper(helpr.left, data);
+      } else if (data > helpr.data) {
+        helpr.right = deleteHelper(helpr.right, data);
+      } else {
+        if (!helpr.left) {
+          return helpr.right;
+        }
+        if (!helpr.right) {
+          return helpr.left;
+        }
+        let rightMin = helpr.right;
+        while (rightMin.left) {
+          rightMin = rightMin.left;
+        }
+        helpr.data = rightMin.data;
+        helpr.right = deleteHelper(helpr.right, rightMin.data);
+      }
+      return helpr;
+    };
+    this.rootHelper = deleteHelper(this.rootHelper, data);
   }
-
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let iterator = this.rootHelper;
+    if (!iterator) {
+      return null;
+    }
+    while (iterator.left) {
+      iterator = iterator.left;
+    }
+    return iterator.data;
   }
-
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let iterator = this.rootHelper;
+    if (!iterator) {
+      return null;
+    }
+    while (iterator.right) {
+      iterator = iterator.right;
+    }
+    return iterator.data;
   }
 }
 
--- src/queue.js
+++ src/queue.js
@@ -13,24 +13,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
+ class Queue {
+  constructor() {
+    this.begin = null;
+    this.last = null;
+  }
+  static ListQueue = class {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  };
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.begin;
   }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const newElem = new Queue.ListQueue(value); 
+    if (!this.last) {
+      this.begin = this.last = newElem;
+    } else {
+      this.last.next = newElem;
+      this.last = newElem;
+    }
   }
-
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.begin) return null;
+    const agedItem = this.begin.value;
+    this.begin = this.begin.next;
+    if (!this.begin) this.last = null;
+    return agedItem;
   }
 }
-
 module.exports = {
   Queue
 };
